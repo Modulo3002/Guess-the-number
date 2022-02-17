8ac7165b679481970afc3b5e87dc95d2ddfce64e
Modulo3002 <81294929+Modulo3002@users.noreply.github.com>
2022-02-17 16:29:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const welkom = prompt("Welcome! What is your name?");
+
+//functies 
+const getRandomNumberBetween = function (min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+//
+if (welkom != null) {
+    alert("Hey " + welkom );
+}
+
+const guess = prompt("Enter a number between 0 and 25 to start guessing...")
+if (guess === getRandomNumberBetween){
+    const correct = alert("Congratulations, you have won the game!");
+}
+else {
+    const helaas = prompt("Helaas probeer opnieuw, je hebt nog 4 kansen over");
+    if (guess === getRandomNumberBetween){
+        const correct = alert("Congratulations, you have won the game!");
+    }
+    else {
+        const helaas = prompt("Helaas probeer opnieuw, je hebt nog 3 kansen over");
+        if (guess === getRandomNumberBetween){
+            const correct = alert("Congratulations, you have won the game!");
+        }
+        else {
+            const helaas = prompt("Helaas probeer opnieuw, je hebt nog 2 kansen over");
+            if (guess === getRandomNumberBetween){
+                const correct = alert("Congratulations, you have won the game!");
+            }
+            else {
+                const helaas = prompt("Helaas probeer opnieuw, je hebt nog 1 kans over");
+                if (guess === getRandomNumberBetween){
+                    const correct = alert("Congratulations, you have won the game!");
+                }
+                else verloren = alert("Helaas, je hebt verlopen!")
+            }
+
+                
+    }
+}
+}
+const bye = alert("Bye" + welkom);
+
+
+
+// te doen!
+// nummer berekenen tussen 0 en 25
+// 
+// 
+// 
+// const incorrect = alert("Enter a number between 0 and 25 to start guessing...");
+// const correct = alert("Congratulations, you have won the game!");
+// const bye = alert("Bye " + welkom);
